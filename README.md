681832b77897ee6044ba45a644299397673d91f0
Gé Koerkamp <ge.koerkamp@gmail.com>
2022-02-17 21:38:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Build your own linux kernel
+Copyright (c) 2022 Gé Koerkamp / ge.koerkamp@volum##.com
+
+## Intro
+This script is used for building the x86 kernel used for Volumio 3.
+
+## Prerequisites
+
+This build process has been tested on Debia Buster (Debian 10), but should work on any Ubuntu >= 20.xy  
+You will need the following minimal packages:
+
+```
+build-essential linux-source bc kmod flex cpio libncurses5-dev libelf-dev
+```
+
+## Process
+
+### Clone the build repository
+
+```
+git clone https://github.com/gkkpch/volumio-x86-build-kernel --depth 1
+```
+### Run the build process
+
+```
+cd volumio-x86-build-kernel
+./buildx86kernel.sh
+```
---
+++ scripts/main.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+log "Using these configuration parameters" "info"
+log "INSTALL_MOD_STRIP: ${INSTALL_MOD_STRIP}" "cfg"
+log "KERNELDIR: ${KERNELDIR}" "cfg"
+log "PATCHDIR: ${PATCHDIR}" "cfg"
+log "PLATFORMDIR: ${PLATFORMDIR}" "cfg"
+
+
+log "KERNELREPO: ${KERNELREPO}" "cfg"
+log "KERNELBRANCH: ${KERNELBRANCH}" "cfg"
+log "LOCALVERSION: ${LOCALVERSION}" "cfg"
+
+source "${SRC}"/scripts/functions.sh
+
+get_kernel_repo
+get_platform_repo
+
+cd $KERNELDIR
+clean_previous_builds
+
+get_latest_kernel
+
+add_additional_sources
+
+add_user_patches
+
+
+log "Now ready for additional sources and patches" "Info"
+read -p "Press [Enter] key to resume ..."
+
+git diff > $PATCHDIR/volumio-kernel.patch
+
+kernel_config
+
+compile_kernel
+
+move_to_storage
+
+exit
+
+
+
